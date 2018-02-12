972c41bdd3a8705b648867e7a7481a0f66d446dd
Humberto Henriquez <humbertohenriquez@Humbertos-MacBook-Pro.local>
2018-02-11 20:28:28 -0500

    second commit

---
+++ Flix_App/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixApp
+//
+//  Created by Humberto Henriquez on 2/7/18.
+//  Copyright © 2018 Austin Edwards. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
